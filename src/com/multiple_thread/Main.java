de21731929979c52c4cdfa6a6fd95616deff3c47
sonu mahto <sonumahtoraj121@gmail.com>
2023-03-28 22:55:22 +0530

    Some changes

--- src/com/multiple_thread/Main.java
+++ src/com/multiple_thread/Main.java
@@ -3,10 +3,11 @@ import static com.multiple_thread.ThreadColor.*;
 public class Main {
     public static void main(String[] args) {
         countdown ct1=new countdown();
+        countdown ct2=new countdown();
         countdownthread t1=new countdownthread(ct1);
         t1.setName("Thread 1");
         //t1.wait(1000);
-        countdownthread t2=new countdownthread(ct1);
+        countdownthread t2=new countdownthread(ct2);
         t2.setName("Thread 2");
         t1.start();
         t2.start();
@@ -25,9 +26,12 @@ class countdown{
             default:
                 color= ThreadColor.ANSI_GREEN;
         }
-        for(int i=10;i>=0;i--){
-            System.out.println(color + Thread.currentThread().getName() + ":"+i);
+        synchronized (color){
+            for(int i=10;i>=0;i--){
+                System.out.println(color + Thread.currentThread().getName() + ":"+i);
+            }
         }
+
     }
 }
 class countdownthread extends Thread{
